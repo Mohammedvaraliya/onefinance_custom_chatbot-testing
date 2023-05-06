97aec5c930b7d134f95be73dad1ad04e21119674
Mohammedvaraliya <mohammedvaraliya2661392@gmail.com>
2023-05-06 16:10:15 +0530

    added faq data as custom database to train on our model

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from llama_index import SimpleDirectoryReader, GPTListIndex, readers, GPTVectorS
 from langchain import OpenAI
 import sys
 import os
+import jsonpickle
 
 def construct_index(directory_path):
     # set maximum input size
@@ -24,22 +25,27 @@ def construct_index(directory_path):
     service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor, prompt_helper=prompt_helper)
     index = GPTVectorStoreIndex.from_documents(documents, service_context=service_context)
 
-    index.save_to_disk('index.json')
+    with open('index.json', 'w') as f:
+        f.write(jsonpickle.encode(index))
 
     return index
 
 def ask_ai():
-    index = GPTVectorStoreIndex.load_from_disk('index.json')
+    with open('index.json', 'r') as f:
+        index = jsonpickle.decode(f.read())
+
     while True: 
         query = input("What do you want to ask? ")
-        response = index.query(query)
-        print(f"Response: {response.response}")
-
-
-
+        response = index.query(query, k=1)
+        if response:
+            print(f"Response: {response[0].text}")
+        else:
+            print("Sorry, I don't have an answer to that question.")
+        print(f"Response: {response}")
 
 if __name__ == "__main__":
 
     os.environ["OPENAI_API_KEY"] = input("Paste your OpenAI key here and hit enter:")
     construct_index("context_data/data")
     ask_ai()
+
