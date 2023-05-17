ea2b44801d1947c51c7e12e7484a354a5197f82a
Mohammedvaraliya <mohammedvaraliya2661392@gmail.com>
2023-05-17 16:23:12 +0530

    update model and data

--- main.py
+++ main.py
@@ -5,19 +5,19 @@ import os
 
 def construct_index(directory_path):
     # set maximum input size
-    max_input_size = 4096
+    max_input_size = 1024
     # set number of output tokens
-    num_outputs = 2000
+    num_outputs = 1000
     # set maximum chunk overlap
-    max_chunk_overlap = 20
+    max_chunk_overlap = 10
     # set chunk size limit
-    chunk_size_limit = 600 
+    chunk_size_limit = 512 
 
     # define prompt helper
     prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
 
     # define LLM
-    llm_predictor = LLMPredictor(llm=OpenAI(temperature=0, model_name="text-davinci-003", max_tokens=num_outputs))
+    llm_predictor = LLMPredictor(llm=OpenAI(temperature=0, model_name="text-davinci-002", max_tokens=num_outputs))
  
     documents = SimpleDirectoryReader(directory_path).load_data()
     
