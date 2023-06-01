b14ad7b40ad54050a5c0a5ff599298f1f4a3ab6b
Mohammedvaraliya <mohammedvaraliya2661392@gmail.com>
2023-06-01 16:00:24 +0530

    update max_token of input and output

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ import os
 
 def construct_index(directory_path):
     # set maximum input size
-    max_input_size = 1024
+    max_input_size = 500
     # set number of output tokens
-    num_outputs = 1000
+    num_outputs = 500
     # set maximum chunk overlap
     max_chunk_overlap = 10
     # set chunk size limit
@@ -19,7 +19,7 @@ def construct_index(directory_path):
     prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
 
     # define LLM
-    llm_predictor = LLMPredictor(llm=ChatOpenAI(temperature=0.7, model_name="text-davinci-003", max_tokens=num_outputs))
+    llm_predictor = LLMPredictor(llm=ChatOpenAI(temperature=0.7, model_name="gpt-3.5-turbo", max_tokens=num_outputs))
  
     documents = SimpleDirectoryReader(directory_path).load_data()
     
@@ -33,23 +33,17 @@ def construct_index(directory_path):
 
 def ask_ai():
     index = GPTSimpleVectorIndex.load_from_disk('index.json')
-    llm = OpenAI(temperature=0.7, model_name="text-davinci-003")
-    memory = ConversationBufferMemory(return_messages=True)
-    conversation = ConversationChain(llm=llm, memory=memory)
+    memory = ConversationBufferMemory()
+    previous_response = ""
 
     while True:
         query = input("Ask anything about 1 Finance : ")
         if not query:
             print("Please enter something to get the response")
         else:
-            memory.chat_memory.add_user_message(query)
-            query_config = {
-                'history': [memory.chat_memory]
-            }
             response = index.query(query).response
-            memory.chat_memory.add_ai_message(response)
+            previous_response = response
         print(f"Response: {response}")
-        # print(query_config)
         print("\n")
 
 if __name__ == "__main__":
